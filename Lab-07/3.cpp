cf692cdb4270e025441cc463a28443e52aa78d63
Muhammad Raffey <70153209@student.uol.edu.pk>
2025-12-06 04:20:09 +0000

    refactor: remove redundant comments in merge function

--- Lab-07/3.cpp
+++ Lab-07/3.cpp
@@ -5,18 +5,14 @@ void merge(int arr[], int left, int mid, int right)
 {
     int n1 = mid - left + 1;
     int n2 = right - mid;
-
-    // Create temporary arrays
     int *leftArr = new int[n1];
     int *rightArr = new int[n2];
 
-    // Copy data to temporary arrays
     for (int i = 0; i < n1; i++)
         leftArr[i] = arr[left + i];
     for (int j = 0; j < n2; j++)
         rightArr[j] = arr[mid + 1 + j];
 
-    // Merge the temporary arrays back into arr[left..right]
     int i = 0, j = 0, k = left;
 
     while (i < n1 && j < n2)
@@ -34,7 +30,6 @@ void merge(int arr[], int left, int mid, int right)
         k++;
     }
 
-    // Copy remaining elements of leftArr[], if any
     while (i < n1)
     {
         arr[k] = leftArr[i];
@@ -42,7 +37,6 @@ void merge(int arr[], int left, int mid, int right)
         k++;
     }
 
-    // Copy remaining elements of rightArr[], if any
     while (j < n2)
     {
         arr[k] = rightArr[j];
@@ -50,7 +44,6 @@ void merge(int arr[], int left, int mid, int right)
         k++;
     }
 
-    // Free temporary arrays
     delete[] leftArr;
     delete[] rightArr;
 }
@@ -61,11 +54,9 @@ void mergeSort(int arr[], int left, int right)
     {
         int mid = left + (right - left) / 2;
 
-        // Sort first and second halves
         mergeSort(arr, left, mid);
         mergeSort(arr, mid + 1, right);
 
-        // Merge the sorted halves
         merge(arr, left, mid, right);
     }
 }
